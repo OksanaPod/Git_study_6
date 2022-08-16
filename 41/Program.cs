d177da73342e3e97d5003edf01955f3864fc6145
OksanaP <77opod@gmail.com>
2022-08-16 09:08:33 +0300

    first commit

---
+++ 41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите сколько чисел проверяем =");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[M];
+int s = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if (arr[i] > 0) { s = s+1; }
+}
+Console.WriteLine("\n Чисела больше 0 = {0}", s);
